684c3077a3fb0e83afcd8495e86c0ba46872720a
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-04-08 11:15:34 +0200

    modulo HTTP

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-import saluta from './utils.js'
-import nomi from './nomi.js'
-import os from 'os'
-import path from 'path'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import http from 'http'
 
-saluta(nomi.persona1)
-saluta('Anna')
-saluta(nomi.persona2)
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('benvenuto sul sito')
+    return
+  }
+  if (req.url === '/francesco') {
+    res.end('il profilo di francesco')
+    return
+  }
+  res.end(`
+   <h1>Errore</h1>
+   <p>Torna alla <a href="/"> home</a> </p>
+   `)
+})
 
-//questi console.log danno informazioni sul computer che sto utilizzando
-console.log(os.userInfo())
-console.log(os.uptime())
-console.log(os.version())
-
-console.log('Sono un path.sep: ', path.sep)
-const percorsoFile = path.join('/cartella', 'sottocartella', 'prova.txt')
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const percorsoAssoluto = path.resolve(
-  __dirname,
-  'cartella',
-  'sottocartella',
-  'prova.txt'
-)
-
-console.log(percorsoAssoluto)
+server.listen(3000)
